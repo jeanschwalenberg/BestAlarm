ff202bcdac3a1f4fc9b3785bf2f787d9eb034683
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-01-28 02:01:58 -0700

    MY Initial Commit
    + model User.cs
    + HomeController.cs [UserMaker *not* complete]

---
+++ BestAlarm/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BestAlarm.Models;
+
+namespace BestAlarm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult UserMaker() {
+            User babyUser = new User() {
+
+            }
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BestAlarm/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestAlarm.Models {
+    public class User {
+        //This is my base class. I think.
+        //...
+        //This is my business... my starting point/most reductive whatever.
+        public string UserID { get; set; }
+    }
+}
